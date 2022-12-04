0f7035de28c2ae30831563f576324f15f3111555
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-04 15:59:30 +0530

    Create avd_of_numbers.c

---
+++ avd_of_numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,tot=2;
+    float avg;
+    printf("average of two numbers:-\n");
+    scanf("%d %d",&a,&b);
+    avg=((a+b)/tot);
+    printf("%f average of two numbers is=",avg);
+}    
