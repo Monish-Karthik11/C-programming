7cf6fdb599688c1134d3d038797e7354926176bf
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-04 13:55:17 +0530

    Create add_sub_of_two_numbers.c

---
+++ add_sub_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("addition and subraction of two numbers\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("addition of %d and %d is %d \n",a,b,a+b);
+    printf("subraction of %d and %d is %d",a,b,a-b);
+    
+}
