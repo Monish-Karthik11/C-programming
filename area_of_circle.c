9cd3315850b7fb227fa539eca79f6b6e1ea3afa4
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-04 15:45:34 +0530

    Create area_of_circle.c

---
+++ area_of_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define pie 3.14
+void main()
+{
+    float r,area;
+    printf("area of circle:-\n");
+    scanf("%f",&r);
+    area=pie*r*r;
+    printf("area of circle: %.3f",area);
+}
