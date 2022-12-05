ec0b2735d7b0ad134e3970325fafa351903b3387
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-05 22:23:04 +0530

    Create even_odd_using_if.c

---
+++ even_odd_using_if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a number\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("%d is a even no",a);
+    }
+    else
+    {
+        printf("%d is odd no",a);
+    }
+}
