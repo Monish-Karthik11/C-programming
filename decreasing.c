53fe76b722e1bfddaef9671731cc1de8fe3e7122
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-09 22:50:21 +0530

    Create decreasing.c

---
+++ decreasing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i,n;
+    printf("print decreasing order\n");
+    printf("enter a number\n");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf(" %d ",i);
+    }
+
+}
