75281f4915659992d260c09eba8484f071775038
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-10 17:55:40 +0530

    Create reverse_natural_num.c

---
+++ reverse_natural_num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("print reverse of a number\n");
+    scanf("%d",&n);
+    while(n>=1)
+    {
+        printf("%d\n",n);
+        n--;
+    }
+}
