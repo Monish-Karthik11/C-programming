e2d4e1bb8f25b106784a97782bbfebfc378b555e
Monish karthik na <101971617+AdithRaj11@users.noreply.github.com>
2022-12-23 22:31:01 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            count=count+1;
+            if(count%2==0)
+            {
+                printf("0 ");
+            }
+            else
+            {
+                printf("1 ");
+            }
+        }
+        printf("\n");
+    }
+}
